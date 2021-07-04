2e85eb5224e288dbea9ac9df148350e60cf9eeae
ShradhaNbr <sradha.nambiar@gmail.com>
2021-07-04 12:50:36 +0530

    Refactored code to print number or times die is rolled and player position

--- SnakeandLadder/src/Snakeandladder.java
+++ SnakeandLadder/src/Snakeandladder.java
@@ -2,6 +2,7 @@ public class Snakeandladder {
 	public static void main(String[] args) {
 		int player_position = 0;
 		final int WINNING_POSITION = 100;
+		int die_count = 0;
 		while (player_position < WINNING_POSITION) {
 			int die_roll = (int) (Math.floor(Math.random() * 10) % 6 + 1);
 			System.out.println("You got" + die_roll);
@@ -9,23 +10,29 @@ public class Snakeandladder {
 			switch (option1) {
 			case 0: {
 				System.out.println("Ladder");
-				if ((player_position + die_roll) > 100)
+				if ((player_position + die_roll) > 100) {
 					player_position = player_position;
-				else
+					die_count++;
+				} else
 					player_position = player_position + die_roll;
+				die_count++;
 				break;
 			}
 			case 1: {
 				System.out.println("Snake");
 				player_position = player_position - die_roll;
+				die_count++;
 				break;
 			}
 			default:
 				System.out.println("You got no play");
+				die_count++;
 				if (player_position < 0)
 					player_position = 0;
-
+				System.out.println("Player is at number" + player_position);
 			}
+
 		}
+		System.out.println(die_count + "times die was rolled");
 	}
 }
