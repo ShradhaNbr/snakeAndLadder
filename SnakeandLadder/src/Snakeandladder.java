a0a9cda7de02f96c7622b911cab3097f95dfe78d
ShradhaNbr <sradha.nambiar@gmail.com>
2021-07-04 12:39:33 +0530

    Refactored code to check Ladder, Snake or No play

--- SnakeandLadder/src/Snakeandladder.java
+++ SnakeandLadder/src/Snakeandladder.java
@@ -1,7 +1,23 @@
 public class Snakeandladder {
 	public static void main(String[] args) {
 		int player_1 = 0;
-		int die_roll = (int) (Math.floor(Math.random() * 10 ) % 6 + 1);
-		System.out.println("You got " +  die_roll);
+		int option;
+		int die_roll = (int) (Math.floor(Math.random() * 10) % 6 + 1);
+		System.out.println("You got" + die_roll);
+		int option1 = (int) (Math.floor(Math.random() * 10) % 3);
+		switch (option1) {
+		case 0: {
+			System.out.println("Ladder");
+			player_1 = player_1 + die_roll;
+			break;
+		}
+		case 1: {
+			System.out.println("Snake");
+			player_1 = player_1 - die_roll;
+			break;
+		}
+		default:
+			System.out.println("You got no play");
+		}
 	}
 }
