925b2b0c3d4f2fd8e20e37546815beaa4946d74c
ShradhaNbr <sradha.nambiar@gmail.com>
2021-07-04 12:36:04 +0530

    Refactored code to roll a die

--- SnakeandLadder/src/Snakeandladder.java
+++ SnakeandLadder/src/Snakeandladder.java
@@ -1,6 +1,7 @@
 public class Snakeandladder {
 	public static void main(String[] args) {
-		int player_1=0;
-		System.out.println("Position of player1 is " +  player_1);
+		int player_1 = 0;
+		int die_roll = (int) (Math.floor(Math.random() * 10 ) % 6 + 1);
+		System.out.println("You got " +  die_roll);
 	}
 }
